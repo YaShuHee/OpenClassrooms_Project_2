073cbadc2acf8179cb9b241283fbf6c49e538d0a
Timothé Gélibert <tim.gelibert@protonmail.com>
2021-03-13 19:20:32 +0100

    Fixes #11, Refactored scraper.py using OOP paradigm

--- src/scraper.py
+++ src/scraper.py
@@ -10,154 +10,164 @@ from bs4 import BeautifulSoup
 
 
 # +--- Decorator imports ----------------------------------------------------+
-from functools import cache
+from functools import cached_property
 
 
-# FUNCTIONS -----------------------------------------------------------------+
-# +--- BeautifulSoup4 manipulations functions -------------------------------+
-def get_soup(url: str) -> BeautifulSoup:
-    """ Send a GET request to the given URL, parse the returned HTML
-    and return a BeautifulSoup object, which can be used to scrape data. """
-    return BeautifulSoup(requests.get(url).content, "html.parser")
+# +--- Os imports -----------------------------------------------------------+
+from os import sep
 
 
-# +--- Extraction functions -------------------------------------------------+
-def extract_product_informations(url: str) -> dict:
-    """ Extract all the product informations from its soup. """
-    soup = get_soup(url)
-    return {
-        "product_page_url": url,
-        **extract_product_title(soup),
-        **extract_product_description(soup),
-        **extract_product_category(soup),
-        **extract_product_review_rating(soup),
-        **extract_product_image_url(soup),
-        **extract_product_infos_from_table(soup),
-    }
+# CLASSES -------------------------------------------------------------------+
+# +--- books.toscrape.com generic scraper class -----------------------------+
+class Scraper:
+    columns_tuple = (
+            "product_page_url",
+            "universal_product_code",
+            "title",
+            "price_including_tax",
+            "price_excluding_tax",
+            "number_available",
+            "product_description",
+            "category",
+            "review_rating",
+            "image_url"
+            )
+    csv_columns_line = ",".join(columns_tuple)
+
+    def __init__(self, url: str):
+        self.url = url
+        self.soup = BeautifulSoup(requests.get(self.url).content, features="html.parser")
+
+    def _quote(string: str) -> str:
+        return f"\"{string}\""
+    
+    def write_csv(self, file_path: str, *info_lines: list):
+        with open(file_path, "w") as file:
+            file.write("\n".join([Scraper.csv_columns_line, *info_lines]))
 
 
-def extract_product_title(soup: BeautifulSoup) -> dict:
-    """ Extract the product title from its soup. """
-    return {"title": soup.find("h1").string}
+# +--- Product Scraper class ------------------------------------------------+
+class ProductScraper(Scraper):
+    def __init__(self, url: str):
+        Scraper.__init__(self, url)
 
+    # extraction private properties ------------------------------------------
+    @cached_property
+    def _extracted_product_informations_from_table(self) -> str:
+        headers_to_extract = ("UPC", "Price (incl. tax)", "Price (excl. tax)", "Availability")
+        th_tags = self.soup.find("table", class_="table table-striped").find_all("th")
+        infos = {th.string: th.find_next("td").string for th in th_tags if th.string in headers_to_extract}
+        return infos
 
-def extract_product_description(soup: BeautifulSoup) -> dict:
-    """ Extract the product description from its soup. """
-    return {"product_description": soup.find("div", id="product_description").find_next("p").string}
+    @cached_property
+    def _extracted_universal_product_code(self) -> str:
+        return self._extracted_product_informations_from_table["UPC"]
+    
+    @cached_property
+    def _extracted_title(self) -> str:
+        return self.soup.find("h1").string
+    
+    @cached_property
+    def _extracted_price_including_tax(self) -> str:
+        return self._extracted_product_informations_from_table["Price (incl. tax)"]
+    
+    @cached_property
+    def _extracted_price_excluding_tax(self) -> str:
+        return self._extracted_product_informations_from_table["Price (excl. tax)"]
+    
+    @cached_property
+    def _extracted_number_available(self) -> str:
+        return self._extracted_product_informations_from_table["Availability"]
+    
+    @cached_property
+    def _extracted_product_description(self) -> str:
+        return self.soup.find("div", id="product_description").find_next("p").string 
+    
+    @cached_property
+    def _extracted_category(self) -> str:
+        return self.soup.find("ul", class_="breadcrumb").find_all("a")[-1].string
+    
+    @cached_property
+    def _extracted_review_rating(self) -> str:
+        return self.soup.find("p", class_="star-rating")["class"][1]
+    
+    @cached_property
+    def _extracted_image_url(self) -> str:
+        return self.soup.find("div", class_="item active").find("img")["src"]
 
-
-def extract_product_category(soup: BeautifulSoup) -> dict:
-    """ Extract the product category from its soup. """
-    return {"category": soup.find("ul", class_="breadcrumb").find_all("a")[-1].string}
-
-
-def extract_product_review_rating(soup: BeautifulSoup) -> dict:
-    """ Extract the product review rating from its soup. """
-    return {"review_rating": soup.find("p", class_="star-rating")["class"][1]}
-
-
-def extract_product_image_url(soup: BeautifulSoup) -> dict:
-    """ Extract the product image URL. """
-    return {
-        "image_url":
-            soup.find("div", class_="item active").find("img")["src"]
-    }
-
-
-def extract_product_infos_from_table(soup: BeautifulSoup) -> dict:
-    """ Extract several product informations from a table in its soup :
-            - universal_product_code,
-            - price_including_tax,
-            - price_excluding_tax,
-            - number_available. """
-    headers_to_extract = ("UPC", "Price (excl. tax)", "Price (incl. tax)", "Availability")
-    th_tags = soup.find("table", class_="table table-striped").find_all("th")
-    infos = {th.string: th.find_next("td").string for th in th_tags if th.string in headers_to_extract}
-    return infos
-
-
-# +--- Transformation functions ---------------------------------------------+
-def transform_product_informations(extracted_infos: dict) -> dict:
-    """ Transform all the previously extracted product informations. """
-    transformed_informations = {
-        "product_page_url":
-            extracted_infos["product_page_url"],
-        "universal_product_code":
-            extracted_infos["UPC"],
-        "title":
-            extracted_infos["title"],
-        "price_including_tax":
-            transform_product_prices(extracted_infos["Price (incl. tax)"]),
-        "price_excluding_tax":
-            transform_product_prices(extracted_infos["Price (excl. tax)"]),
-        "number_available":
-            transform_product_availability(extracted_infos["Availability"]),
-        "product_description":
-            transform_product_description(extracted_infos["product_description"]),
-        "category":
-            extracted_infos["category"],
-        "review_rating":
-            transform_product_review_rating(extracted_infos["review_rating"]),
-        "image_url":
-            transform_product_image_url(extracted_infos["image_url"]),
-    }
-    transformed_informations = {
-        key: quote(value) for key, value in transformed_informations.items()
-    }
-    return transformed_informations
-
-
-def quote(string: str) -> str:
-    return f"\"{string}\""
-
-
-def transform_product_prices(price: str) -> str:
-    """ Format the product prices. """
-    return price[1:]
-
-
-def transform_product_availability(availabilty: str) -> str:
-    """ Format the product availability. """
-    return availabilty.replace("In stock (", "").replace(" available)", "")
-
-
-def transform_product_description(description: str) -> str:
-    """ Format the product description. """
-    return description.replace("\"", "\"\"")
-
-
-@cache  # will calculate only once for each argument
-def transform_product_review_rating(review_rating: str) -> str:
-    """ Format the product review rating. """
-    return {
+    # transform public properties --------------------------------------------
+    @cached_property
+    def universal_product_code(self) -> str:
+        return self._extracted_universal_product_code
+    
+    @cached_property
+    def title(self) -> str:
+        return self._extracted_title
+    
+    @cached_property
+    def price_including_tax(self) -> str:
+        return self._extracted_price_including_tax[1:]
+    
+    @cached_property
+    def price_excluding_tax(self) -> str:
+        return self._extracted_price_excluding_tax[1:]
+    
+    @cached_property
+    def number_available(self) -> str:
+        return self._extracted_number_available.replace("In stock (", "").replace(" available)", "")
+    
+    @cached_property
+    def product_description(self) -> str:
+        return self._extracted_product_description.replace("\"", "\"\"")
+    
+    @cached_property
+    def category(self) -> str:
+        return self._extracted_category
+    
+    @cached_property
+    def review_rating(self) -> str:
+        return {
         "One": "1",
         "Two": "2",
         "Three": "3",
         "Four": "4",
         "Five": "5"
-    }[review_rating]
+    }[self._extracted_review_rating]
+
+    @cached_property
+    def image_url(self) -> str:
+        return self._extracted_image_url.replace("../..", "https://books.toscrape.com")
+
+    # extracted and transformed informations merging -------------------------
+    @cached_property
+    def _informations_dict(self) -> dict:
+        infos_dict = {
+            "product_page_url": self.url,
+            "universal_product_code": self.universal_product_code,
+            "title": self.title,
+            "price_including_tax": self.price_including_tax,
+            "price_excluding_tax": self.price_excluding_tax,
+            "number_available": self.number_available,
+            "product_description": self.product_description,
+            "category": self.category,
+            "review_rating": self.review_rating,
+            "image_url": self.image_url,
+        }
+        return {key: Scraper._quote(value) for key, value in infos_dict.items()}
+
+    @cached_property
+    def csv_informations_line(self) -> str:
+        return ",".join(self._informations_dict.values())
+
+    # load method ------------------------------------------------------------
+    def write_csv(self, directory: str, file_name: str = ""):
+        if file_name == "":
+            file_name = self.title + ".csv"
+        file_path = directory + sep + file_name
+        super().write_csv(file_path, self.csv_informations_line)
 
 
-def transform_product_image_url(url: str) -> str:
-    return url.replace("../..", "https://books.toscrape.com")
-
-
-# +--- Loading functions ----------------------------------------------------+
-def generate_product_informations_csv(transformed_infos: dict) -> dict:
-    """ Generate the product CSV content. """
-    csv_content = ""
-    to_write = ""
-    for column in transformed_infos.keys():
-        to_write += f"{column},"
-    csv_content += to_write[:-2] + "\n"
-    to_write = ""
-    for value in transformed_infos.values():
-        to_write += f"{value},"
-    csv_content += to_write[:-2]
-    return csv_content
-
-
-def write_csv(file_path: str, csv_content: str) -> bool:
-    """ Write the CSV. """
-    with open(file_path, "w") as file:
-        file.write(csv_content)
+# CODE EXECUTION (Test this implementation when runned without being imported)
+if __name__ == '__main__':
+    scraper = ProductScraper("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+    scraper.write_csv("/home/tim/Workspace/OpenClassrooms/02/OpenClassrooms_Project_2/results")
